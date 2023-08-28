62e5cf208f4d1f2c3b49051e7e1522b18060e7b5
DrunkFox <matiasfrancesco04@gmail.com>
2023-08-28 15:37:55 +0200

    add some es6 code

--- es6/src/index.js
+++ es6/src/index.js
@@ -4,11 +4,13 @@
 //limit += 100;
 //limit = 200;
 
-const limit = 100;
+//const limit = 100;
 //limit = 200; 
 
 //console.log(limit);
 
+
+/*
 const emails = ['frodo@email.com', 'drunkfox@email.com', 'strudu@email.com' ];
 
 emails.push('pippo@emails.com');
@@ -16,3 +18,27 @@ console.log(emails);
 
 emails.pop();
 console.log(emails);
+*/
+
+
+/*
+
+const limit = 200;
+
+{
+    //local let
+    const limit = 10;
+    console.log('backstage limit', limit);
+}
+
+console.log('overall venue limit', limit);
+
+*/
+
+let a = `good`;
+let greeting = `${a} morning`;   //I have use `` instead of ''
+console.log(greeting);
+
+let b = 'birthday';
+let c = `happy ${b}`;
+console.log(c);
