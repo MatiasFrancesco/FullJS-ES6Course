94760a210e4298fed4b299160eacd27fee2de4c6
DrunkFox <matiasfrancesco04@gmail.com>
2023-09-04 11:35:32 +0200

    add set and map methods

--- es6_Set_Map/src/index.js
+++ es6_Set_Map/src/index.js
@@ -1,6 +1,6 @@
 /* use of SET */
 
-let numbers = [231,45,332,12,2];
+let numbers = [231, 45, 332, 12, 2];
 let numberSet = new Set(numbers);
 console.log(numberSet);
 
@@ -11,3 +11,42 @@ console.log(numberSet.has(69))
 console.log(numberSet.has(240))
 
 
+/* use of MAP */
+
+let a = new Map();
+let key_1 = "string key";
+a.set(key_1, 'return value for a string key');
+console.log(a);
+
+let key_2 = {
+    a: 'key'
+};
+a.set(key_2, 'return value for a object key');
+console.log(a);
+
+let key_3 = (a,b) => { return a*b};
+a.set(key_3, 'return value for a function key' );
+console.log(a);
+
+//simple use of Map 
+let numArr = [[1, "one"], [2,"two"], [3, "Three"]];
+let valMap = new Map(numArr);
+//console.log(valMap);
+
+for (let [key, value] of valMap.entries()){
+    console.log(`${key} points to ${value}`);
+}
+
+
+//how many letters have a string
+let string = "dasdasjhdgashgjdasjdfgashjkdvaseduawefgd";
+let letters = new Map();
+for (let i = 0; i<string.length; i++){
+    let letter = string[i];
+    if(!letters.has(letter)){
+        letters.set(letter, 1);
+    } else {
+        letters.set(letter, letters.get(letter) + 1);
+    }
+}
+console.log(letters);
