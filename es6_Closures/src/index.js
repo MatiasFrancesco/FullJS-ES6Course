7a066e41c60b4212b64afe28e7e84fa5c8002932
DrunkFox <matiasfrancesco04@gmail.com>
2023-09-06 14:21:34 +0200

    add private methods

--- es6_Closures/src/index.js
+++ es6_Closures/src/index.js
@@ -1,9 +1,9 @@
 let call = () => {
     let secret = 'ES6 rocks!';
-    let reveal = () =>{
+    let reveal = () => {
         console.log(secret);
     }
-return reveal;
+    return reveal;
 }
 //
 //console.log(secret);
@@ -27,13 +27,43 @@ h = add_ness("blind");
 console.log(h);
 
 
-const product = (x) =>{
+const product = (x) => {
     return y => {
-        return y*x;
+        return y * x;
     }
 }
 
-let mult5=product(5);
+let mult5 = product(5);
 console.log(mult5(3));
 let double = mult5(2);
 console.log(double);
+
+
+
+/* PRIVATE METHODS */
+
+const budget = () => {
+    let balance = 0;
+    let changeBal = (val) => {
+        return balance += val;
+    }
+
+    const deposit20 = () => {
+        changeBal(20);
+    }
+
+    const check =()=> balance;
+
+    return {
+        //deposit20: deposit20,
+        //check:check
+        deposit20,
+        check
+    }
+};
+
+let wallet = budget();
+console.log(wallet);
+wallet.deposit20();
+console.log(wallet.check());
+
